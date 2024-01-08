b5938a17ecb61a41b35fb8f7f6c3e466ed0da8db
fehizoroRak <tsimbina@hotmail.fr>
2024-01-08 23:46:44 +0100

    Param Converter Challenge 9

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,15 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Episode;
 use App\Entity\Program;
+use App\Entity\Season;
 use App\Repository\ProgramRepository;
+use App\Repository\SeasonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProgramController extends AbstractController
 {
-
     #[Route('/', name: 'program_index')]
     public function index(ProgramRepository $programsRepository): Response
     {
@@ -20,18 +22,85 @@ class ProgramController extends AbstractController
         ]);
     }
 
+    // #[Route('/show/{id}', name: 'program_show', methods: ['GET'], requirements: ['id' => '^\d+$'], options: ['expose' => true])]
+    // public function show(int $id,ProgramRepository $programRepository): Response
+    // {
+    //     $program = $programRepository->find($id);
+    //     $seasons = $program->getSeasons();
+
+    // if (!$program) {
+    //     throw $this->createNotFoundException(
+    //         'No program with id : '.$id.' found in program\'s table.'
+    //     );
+    // }
+
+    //     return $this->render('program/show.html.twig', [
+    //         'program' => $program,
+    //         'seasons' => $seasons,
+    //     ]);
+    // }
+
+     // PARAM CONVERTER
+     // Installer ceci avant: symfony composer require sensio/framework-extra-bundle
     #[Route('/show/{id}', name: 'program_show', methods: ['GET'], requirements: ['id' => '^\d+$'], options: ['expose' => true])]
-    public function show(int $id,ProgramRepository $programRepository): Response
+    public function show(Program $program, Season $seasons): Response
     {
-        $program = $programRepository->find($id);
+       
+        $seasons = $program->getSeasons();
+
     if (!$program) {
         throw $this->createNotFoundException(
-            'No program with id : '.$id.' found in program\'s table.'
+            'No program with id : {id} found in program\'s table.'
         );
     }
 
         return $this->render('program/show.html.twig', [
             'program' => $program,
+            'seasons' => $seasons,
+        ]);
+    }
+
+
+
+    // #[Route('/program/{programId}/seasons/{seasonId}', name: 'program_season_show')]
+    // public function showSeason(int $programId, int $seasonId, ProgramRepository $programRepository, SeasonRepository $seasonRepository)
+    // {
+    //     $program = $programRepository->find($programId);
+    //     $season = $seasonRepository->find($seasonId);
+    //     $episodes = $season->getEpisodes();
+
+    //     return $this->render('program/season_show.html.twig', [
+    //         'program' => $program,
+    //         'season' => $season,
+    //         'episodes' => $episodes,
+    //     ]);
+    // }
+
+
+    // PARAM CONVERTER
+    // Installer ceci avant: symfony composer require sensio/framework-extra-bundle
+    #[Route('/program/{program}/seasons/{season}', name: 'program_season_show')]
+    public function showSeason(Program $program, Season $season, Episode $episodes)
+    {
+    
+        $episodes = $season->getEpisodes();
+
+        return $this->render('program/season_show.html.twig', [
+            'program' => $program,
+            'season' => $season,
+            'episodes' => $episodes,
+        ]);
+    }
+
+
+    #[Route('/program/{program}/season/{season}/episodes/{episodes}', name: 'program_episode_show')]
+    public function showEpisode(Program $program, Season $season, Episode $episodes)
+    {
+    
+        return $this->render('program/episode_show.html.twig', [
+            'program' => $program,
+            'season' => $season,
+            'episodes' => $episodes,
         ]);
     }
 
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -3,15 +3,24 @@
 namespace App\Entity;
 
 use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
 class Category
 {
+    public function __construct()
+    {
+        $this->programs = new ArrayCollection();
+    }
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
+
+    #[ORM\OneToMany(targetEntity: Program::class, mappedBy: "category")]
+    private $programs;
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $name = null;
@@ -29,6 +38,44 @@ class Category
     public function setName(?string $name): static
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Program[]
+     */
+    public function getPrograms(): Collection
+    {
+        return $this->programs;
+    }
+
+    /**
+     * @param Program $program
+     * @return Category
+     */
+    public function addProgram(Program $program): self
+    {
+        if (!$this->programs->contains($program)) {
+            $this->programs[] = $program;
+            $program->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param Program $program
+     * @return Category
+     */
+    public function removeProgram(Program $program): self
+    {
+        if ($this->programs->removeElement($program)) {
+            // set the owning side to null (unless already changed)
+            if ($program->getCategory() === $this) {
+                $program->setCategory(null);
+            }
+        }
 
         return $this;
     }
